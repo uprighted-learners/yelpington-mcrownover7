dfafcad3ba9070e05a429a0dea1036dfd0f5d15a
Matthew Crownover <matthew.j.crownover@gmail.com>
2021-11-04 13:40:45 -0400

    Now fetches are functioning.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,78 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Map from "./components/Map";
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   const [center, setCenter] = useState([40.4406, -79.9959]);
+  const [restaurantDir, setRestaurantDir] = useState([]);
+
+  const [markerLocations, setMarkerLocations] = useState([]);
+  const [markerKey, setMarkerKey] = useState([]);
+  // const [restaurantName, setRestaurantName] = useState("");
+  // const [restaurantLat, setRestaurantLat] = useState("");
+  // const [restaurantLon, setRestaurantLon] = useState("");
 
   useEffect(() => {
-    fetch("");
-  });
+    fetch("/api")
+      .then((res) => res.json())
+      .then((restDirArray) => {
+        setRestaurantDir(restDirArray);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   let tempName;
+  //   let tempLat;
+  //   let tempLon;
+  //   restaurantDir.forEach((element) => {
+  //     fetch(`/api/${element}`)
+  //       .then((res) => res.json())
+  //       .then((restArray) => {
+  //         // setRestaurantName(restArray.name);
+  //         // setRestaurantLat(restArray.latitude);
+  //         // setRestaurantLon(restArray.longitude);
+  //         tempName = restArray.name;
+  //         tempLat = restArray.latitude;
+  //         tempLon = restArray.longitude;
+  //         console.log(restArray.name);
+  //         markerKey.push(tempName);
+  //         markerLocations.push([tempLat, tempLon]);
+  //       });
+  //     // setMarkerLocations(markerLocations.push([tempLat, tempLon]));
+  //     // setMarkerKey(markerKey.push(tempName));
+  //   });
+  // }, [restaurantDir]);
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(markerKey);
+    console.log(markerLocations)
+  };
+
   return (
     <div>
-      <h1>Yelpington</h1>
+      <h1>Yelpsburgh</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
-          paddingBottom: "1rem"
+          paddingBottom: "1rem",
         }}
       >
         {/* <Link to="/restaurant">Restaurant</Link> */}
       </nav>
+      <form onSubmit={onSubmit}>
+        <input type="submit" />
+      </form>
       {/* Outlet allows for the use of the nested routes in index.js */}
       {/* <Outlet /> */}
-      <Map center={center} setCenter={setCenter} />{" "}
+      <Map
+        center={center}
+        setCenter={setCenter}
+        restaurantDir={restaurantDir}
+        markerKey={markerKey}
+        markerLocations={markerLocations}
+      />{" "}
     </div>
   );
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,35 @@
-import { MapContainer, TileLayer} from "react-leaflet";
-
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useEffect, useState } from "react";
 
 function Map(props) {
+  // const [markerLocations, setMarkerLocations] = useState([]);
+  // const [markerKey, setMarkerKey] = useState([]);
+  // const [restaurantName, setRestaurantName] = useState("");
+  // const [restaurantLat, setRestaurantLat] = useState("");
+  // const [restaurantLon, setRestaurantLon] = useState("");
+
+  useEffect(() => {
+    let tempName;
+    let tempLat;
+    let tempLon;
+    props.restaurantDir.forEach((element) => {
+      fetch(`/api/${element}`)
+        .then((res) => res.json())
+        .then((restArray) => {
+          // setRestaurantName(restArray.name);
+          // setRestaurantLat(restArray.latitude);
+          // setRestaurantLon(restArray.longitude);
+          tempName = restArray.name;
+          tempLat = restArray.latitude;
+          tempLon = restArray.longitude;
+          console.log(restArray.name);
+          props.markerKey.push(tempName);
+          props.markerLocations.push([tempLat, tempLon]);
+        });
+      // setMarkerLocations(markerLocations.push([tempLat, tempLon]));
+      // setMarkerKey(markerKey.push(tempName));
+    });
+  }, [props.restaurantDir]);
 
   return (
     <MapContainer
@@ -13,6 +41,7 @@ function Map(props) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      {/* <Marker key={props.markerKey[0]} position={props.markerLocations[0]} /> */}
     </MapContainer>
   );
 }
